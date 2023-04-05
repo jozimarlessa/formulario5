be8986a481c88b33183872889c28159f319a0d85
jozimar <jozimarlessa78@gmail.com>
2023-04-05 14:45:46 -0300

    ok

--- index.php
+++ index.php
@@ -8,9 +8,23 @@
 </head>
 <body>
     <?php
+    $n= isset($_GETI["num"]) ?$_GET["num"] : 0;
+    $o= isset($_GET["oper"]) ?$_GET["oper"] : 1;
+    switch ($o){
+        case 1:
+            $r = $n * 2;  
+            break; 
+        case 2:
+            $r = $n ^ 3;
+            break;
+        case 3:
+            $r = sqrt($n);
+            break;
+    }
 
-    
+echo (" o resultado da relacao solicitada foi <span class='foco'> $r</span>") 
     ?>
-    
+    <a href="index.html">Voltar</a>
+ <div>   
 </body>
 </html>
